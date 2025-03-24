6ee1f4529c8f397a840d2dce4b404033cf59ec5b
Joanice Matos dos Reis <113264739+joanicematos@users.noreply.github.com>
2025-03-24 12:58:48 -0300

    Initial commit

---
+++ src/test/java/com/reis/SantanderDevWeek2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
