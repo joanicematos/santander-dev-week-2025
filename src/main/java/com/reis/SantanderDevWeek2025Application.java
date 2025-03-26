5617c44c0cdb83611c25c95964edcb467ea3e821
Joanice Matos dos Reis <113264739+joanicematos@users.noreply.github.com>
2025-03-25 22:07:13 -0300

    Bug CORS

--- src/main/java/com/reis/SantanderDevWeek2025Application.java
+++ src/main/java/com/reis/SantanderDevWeek2025Application.java
@@ -1,8 +1,12 @@
 package com.reis;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderDevWeek2025Application {
 
